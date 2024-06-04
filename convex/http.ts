eb230a778fd7b4b9eee5f47c0bae88f10c5f5ccf
Sarah Shader <sarahshader@gmail.com>
2024-06-04 11:38:02 -0400

    Use Convex + Hono to power chat app

--- convex/http.ts
+++ convex/http.ts
@@ -1,11 +1,20 @@
-import { httpRouter } from "convex/server";
-import { httpAction } from "./_generated/server";
+import { Hono } from "hono";
+import { HonoWithConvex, HttpRouterWithHono } from "convex-helpers/server/hono";
+import { ActionCtx } from "./_generated/server";
+import { cors } from "hono/cors";
 import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
-export const chat = httpAction(async (ctx, req) => {
+const app: HonoWithConvex<ActionCtx> = new Hono();
+
+// See the [guide on Stack](https://stack.convex.dev/hono-with-convex)
+// for tips on using Hono for HTTP endpoints.
+
+app.use("/api/*", cors());
+
+app.post("/api/chat", async (c) => {
   // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+  const { messages } = await c.req.json();
 
   // Call the language model
   const result = await streamText({
@@ -19,48 +28,7 @@ export const chat = httpAction(async (ctx, req) => {
   });
 
   // Respond with the stream
-  return result.toAIStreamResponse({
-    headers: {
-      // e.g. https://mywebsite.com, configured on your Convex dashboard
-      "Access-Control-Allow-Origin": "*",
-      Vary: "origin",
-    },
-  });
+  return result.toAIStreamResponse();
 });
 
-const http = httpRouter();
-
-http.route({
-  path: "/api/chat",
-  method: "POST",
-  handler: chat,
-});
-
-http.route({
-  path: "/api/chat",
-  method: "OPTIONS",
-  handler: httpAction(async (_, request) => {
-    // Make sure the necessary headers are present
-    // for this to be a valid pre-flight request
-    const headers = request.headers;
-    if (
-      headers.get("Origin") !== null &&
-      headers.get("Access-Control-Request-Method") !== null &&
-      headers.get("Access-Control-Request-Headers") !== null
-    ) {
-      return new Response(null, {
-        headers: new Headers({
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST",
-          "Access-Control-Allow-Headers": "Content-Type, Digest",
-          "Access-Control-Max-Age": "86400",
-        }),
-      });
-    } else {
-      return new Response();
-    }
-  }),
-});
-
-// Convex expects the router to be the default export of `convex/http.js`.
-export default http;
+export default new HttpRouterWithHono(app);
